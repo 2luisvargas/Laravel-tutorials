2cdf680d4f070525318c7b7522c913b8104f4678
2luisvargas <2luisvargas@gmail.com>
2014-02-25 02:23:56 -0430

    Proyecto 1 listado de usuarios dinámico con base de datos

---
+++ Proyecto1 - codehero gestion de usuarios/app/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>hola</title>
+</head>
+<body>
+	
+
+	@section('sidebar')
+		codehero laravel 4 gestion de usuarios-
+	@show
+	
+	<div class="container">
+		
+		@yield('content')
+
+	</div>	
+
+
+
+</body>
+</html>
